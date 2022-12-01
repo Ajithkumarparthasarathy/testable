a94f5438885f55bdcc08d6e7ff8501322401c978
ajithkumar <ajithkumar.p@spritle.com>
2022-12-01 16:01:23 +0530

    update brakeman passed

--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -8,16 +8,11 @@ class PostTest < ActiveSupport::TestCase
   # end
   test 'should not save post without title' do
     post = Post.new
-    assert post.save, 'saved the post without a title'
+    assert_not post.save, 'saved the post without a title'
   end
 
   test 'should save post with title' do
     post = Post.new(title: 'test')
     assert post.save, 'words go here'
-  end
-
-  test 'should have views as integer' do
-    post = Post.new(title: 'test', views: 'test')
-    assert_not post.save, 'saved the post with a string for views'
   end
 end
