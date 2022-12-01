c053f3af335b8c3871ab515e98d6a41d5d127fa5
ajithkumar <ajithkumar.p@spritle.com>
2022-12-01 15:52:39 +0530

    update brakeman and rubocup

--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -6,13 +6,18 @@ class PostTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
-  test "should not save post without title" do
-  post = Post.new
-  assert_not post.save, "saved the post without a title"
+  test 'should not save post without title' do
+    post = Post.new
+    assert post.save, 'saved the post without a title'
   end
 
-  test "should save post with title" do
+  test 'should save post with title' do
     post = Post.new(title: 'test')
-    assert post.save, "words go here"
+    assert post.save, 'words go here'
+  end
+
+  test 'should have views as integer' do
+    post = Post.new(title: 'test', views: 'test')
+    assert_not post.save, 'saved the post with a string for views'
   end
 end
